76c7cd782711cd5860ba8a4fc61cd9a30c7424f2
Scott Franklin <stfranklin@wisc.edu>
2017-03-08 06:16:17 -0800

    initial commit for homework 2 core files

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,6 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-
-// for SQLite persistence
-// https://www.thepolyglotdeveloper.com/2014/11/use-sqlite-instead-local-storage-ionic-framework/
-// http://nextflow.in.th/en/2015/easy-way-to-work-with-sqlite-database-in-ionic-framework/
-// http://ngcordova.com/docs/install/
-var db;
 var starter = angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform, $cordovaSQLite) {
@@ -26,45 +20,56 @@ var starter = angular.module('starter', ['ionic', 'ngCordova'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-    var db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' }, function (db) {
-
-      // Here, you might create or open the table.
-
-    }, function (error) {
-      console.log('Open database ERROR: ' + JSON.stringify(error));
-    });
-    $cordovaSQLite.execute(db, 'CREATE TABLE IF NOT EXISTS Messages (id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
-      'firstName TEXT, lastName TEXT, phoneNumber TEXT)');
-
   });
 })
 
+// The start of the single controller that we will be using for this lab
+// It is called "mainCtrl" and is connected to the Angular module "starter"
 starter.controller('mainCtrl', function($scope, $ionicModal, $ionicLoading) {
-  $scope.firstName = "";
-  $scope.lastName = "";
-  $scope.phoneNumber = "";
-  $scope.email = "";
-  $scope.listOfPeople = {};
+  $scope.firstName = ""; // Create first name string variable on controller $scope
+  $scope.lastName = ""; // Create last name string variable on controller $scope
+  $scope.phoneNumber = ""; // Create phone number string variable on controller $scope
+  $scope.email = ""; // Create email string variable on controller $scope
+  $scope.listOfPeople = {}; // Create list of people dictionary variable on controller $scope
 
-  $scope.onSubmit = function () {
+  $scope.onSubmit = function () { // Create onSubmit function
+    // This function will be run every time the submit button is pressed
+
+    // Create a variable that is an empty dictionary called "person"
+    // It will hold the persons attributes such as first name, last name, etc
     var person = {};
+
+    // Create an ID variable by appending all the variables that define a person (listed above) as a string
+    // What this does is create a unique identifier for each person by using all the persons attributes to generate a
+    // unique string
     person.id = $scope.firstName+$scope.lastName+$scope.phoneNumber+$scope.email;
-    person.firstName = $scope.firstName;
-    person.lastName = $scope.lastName;
-    person.phoneNumber = $scope.phoneNumber;
-    person.email = $scope.email;
-    $scope.listOfPeople[$scope.firstName+$scope.lastName+$scope.phoneNumber+$scope.email] = person;
+
+    /* Insert your code for the following variables below */
+    person.firstName = ;
+    person.lastName = ;
+    person.phoneNumber = ;
+    person.email = ;
+
+    // Now, add the person variable you added attributes to above to the "listOfPeople" dictionary
+    /* HINT: Use the person variables ID as the "key" value to the dictionary */
+    /* YOUR CODE HERE; 1 line */
+
+    // This function call displays a popover that says "Person Added!"
+    // It is run every time someone presses the submit button and the onSubmit function runs, as it is nested within the
+    // onSubmit function
     $ionicLoading.show({ template: 'Person Added!', noBackdrop: true, duration: 1000 });
   };
 
+  // This function is called everytime a
   $scope.deletePerson = function(person) {
-    var toRemove = [];
-    angular.forEach($scope.listOfPeople, function(value, key) {
-      if (angular.equals(key,person.id)) {
-        this.push(key);
-      }
-    }, toRemove);
-    delete $scope.listOfPeople[toRemove[0]];
+    // What the code is supposed to do is delete a "person" variable from the "listOfPeople" dictionary
+    // This needs to be done using a unique identifier for each "person" variable
+    /* Insert your code to delete a person variable from the dictionary "listOfPeople" */
+    delete $scope.listOfPeople[/* YOUR CODE HERE*/];
+
+    // This function call displays a popover that says "Person Deleted!"
+    // It is run every time someone clicks/presses a person in the list of people and the deletePerson
+    // function runs, as it is nested within the deletePerson function
     $ionicLoading.show({ template: 'Person Deleted!', noBackdrop: true, duration: 1000 });
   };
 
